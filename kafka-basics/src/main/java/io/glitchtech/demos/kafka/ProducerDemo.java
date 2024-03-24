ea508370de8b1bc4bfa634d7eaea4b86e7d13435
Muzi PH <muzi@glitchtech.co.za>
2024-03-24 15:56:17 +0200

    Add Producer demo

---
+++ kafka-basics/src/main/java/io/glitchtech/demos/kafka/ProducerDemo.java
@@ -0,0 +1,40 @@
+package io.glitchtech.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("testing");
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // set Producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create a Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_topic","hello kafka");
+
+        // send data - async operation
+        producer.send(producerRecord);
+
+        // flush - send all data and block until done - sync operation
+        producer.flush();
+
+        // close calls flush automatically
+        producer.close();
+    }
+}
