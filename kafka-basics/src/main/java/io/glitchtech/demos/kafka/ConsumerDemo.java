d25aca8dd251df8602c0fa8e6214f5adebe07345
Muzi PH <muzi.phage@gmail.com>
2024-03-27 10:53:28 +0200

    ConsumerDemo

---
+++ kafka-basics/src/main/java/io/glitchtech/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,52 @@
+package io.glitchtech.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("I am a Kafka Consumer!!");
+        String groupId = "kafka-consumer application";
+        String topic = "demo_topic";
+
+        // create a Consumer Properties
+        Properties properties = new Properties();
+
+        // connect to localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // create Consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create a Consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe to a topic or Collection of topics
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for data
+        while (true) {
+            logger.info("Polling");
+            // poll with timeout -> retrieve if there is data or wait 1 second
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + " - Value: " + record.value());
+                logger.info("Partition: " + record.partition() + " - Offset: " + record.offset());
+            }
+        }
+    }
+}
