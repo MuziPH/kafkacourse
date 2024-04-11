051093b66eaac6ad884ba33b35afa83613524b5c
Muzi <muzi.phage@gmail.com>
2024-04-11 12:24:12 +0200

    Added high throughput enhancements

--- kafka-producer-wikimedia/src/main/java/io/glitchtech/demos/kafka/wikimedia/WikimediaProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/glitchtech/demos/kafka/wikimedia/WikimediaProducer.java
@@ -5,6 +5,7 @@ import com.launchdarkly.eventsource.StreamException;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
@@ -16,11 +17,16 @@ public class WikimediaProducer {
         String bootstrapServers = "127.0.0.1:9092";
         // create Producer properties
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", bootstrapServers);
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
         // set Producer properties
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32KB
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         // create a Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
@@ -31,11 +37,10 @@ public class WikimediaProducer {
         BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
         EventSource.Builder eventSource = new EventSource.Builder(URI.create(url));
-        try (BackgroundEventSource backgroundEventSource = new BackgroundEventSource.Builder(eventHandler, eventSource).build()) {
-
-            // start the producer in another thread
-            backgroundEventSource.start();
-        }
+        BackgroundEventSource backgroundEventSource =
+                new BackgroundEventSource.Builder(eventHandler, eventSource).build();
+        // start the producer in another thread
+        backgroundEventSource.start();
 
         // we produce for 10 minutes and block program
         TimeUnit.MINUTES.sleep(10);
