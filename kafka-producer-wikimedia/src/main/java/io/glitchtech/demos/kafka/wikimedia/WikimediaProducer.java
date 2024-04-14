093668ec5b69c2e05336eb64e6576425bf16c0d6
Muzi <muzi.phage@gmail.com>
2024-04-14 11:23:09 +0200

    Minor adjustments to producer

--- kafka-producer-wikimedia/src/main/java/io/glitchtech/demos/kafka/wikimedia/WikimediaProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/glitchtech/demos/kafka/wikimedia/WikimediaProducer.java
@@ -14,19 +14,18 @@ import java.util.concurrent.TimeUnit;
 
 public class WikimediaProducer {
     public static void main(String[] args) throws StreamException, InterruptedException {
-        String bootstrapServers = "127.0.0.1:9092";
         // create Producer properties
         Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
         // set Producer properties
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // set high throughput producer configs
-        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
-        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32KB
-        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        //properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        //properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32KB
+        //properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         // create a Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
